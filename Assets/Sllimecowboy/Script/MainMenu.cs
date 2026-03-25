62adba0571ca595c5d0148662cfd71c829bc82d6
kunni <nannaphat20160@gmail.com>
2026-03-25 21:40:17 +0700

    Set setting in game scene

--- Assets/Sllimecowboy/Script/MainMenu.cs
+++ Assets/Sllimecowboy/Script/MainMenu.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -14,6 +14,7 @@ public class MainMenu : MonoBehaviour
     {
         panel.SetActive(false);
     }
+
     public void StartGame()
     {
         SceneManager.LoadScene("up");
