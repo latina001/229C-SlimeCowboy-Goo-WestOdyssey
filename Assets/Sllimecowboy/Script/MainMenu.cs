bdbd8102934de4a026c01c26349f66f0828144c8
kunni <nannaphat20160@gmail.com>
2026-03-25 12:06:09 +0700

    Game UI Setting

--- Assets/Sllimecowboy/Script/MainMenu.cs
+++ Assets/Sllimecowboy/Script/MainMenu.cs
@@ -3,6 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject panel;
+
+    public void ShowPanel()
+    {
+        panel.SetActive(true);
+    }
+
+    public void HidePanel()
+    {
+        panel.SetActive(false);
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("up");
