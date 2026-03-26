1414304777a30446052eb3e9973137a073f9d9c3
kunni <nannaphat20160@gmail.com>
2026-03-26 23:44:53 +0700

    Set credit scenes

---
+++ Assets/Sllimecowboy/Script/Menu Script/MoveUpConstant.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MoveUpConstant : MonoBehaviour
+{
+    public float speed = 2f;
+
+    void Start()
+    {
+        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        transform.position +=
+            Vector3.up * speed * Time.unscaledDeltaTime;
+    }
+}
